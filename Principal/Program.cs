9c88a84e53da2d09c0ae7a90aaf3069375c4c542
Victor <viesteba@unirioja.es>
2024-09-30 13:38:56 +0200

    Inicio P03

--- Principal/Program.cs
+++ Principal/Program.cs
@@ -13,21 +13,22 @@ namespace Principal
     {
         static void Main(string[] args)
         {
-            Divisa divRef = new Divisa("dólar", 1);
-            Divisa div1 = new Divisa("euro", 1.11);
-            Divisa div2 = new Divisa("libra", 1.34);
-            Divisa div3 = new Divisa("peso dominicano", 0.017);
-            Divisa div4 = new Divisa("franco suizo", 1.18);
-            ColeccionDivisas colDiv = new ColeccionDivisas();
-            colDiv.Add(divRef);
-            colDiv.Add(div1);
-            colDiv.Add(div2);
-            colDiv.Add(div3);
-            colDiv.Add(div4);
-            Conversor conversor = new Conversor(divRef,colDiv);
-            Consola c = new Consola(conversor);
-            c.Menu();
-            Console.ReadLine();
+            //Divisa divRef = new Divisa("dólar", 1);
+            //Divisa div1 = new Divisa("euro", 1.11);
+            //Divisa div2 = new Divisa("libra", 1.34);
+            //Divisa div3 = new Divisa("peso dominicano", 0.017);
+            //Divisa div4 = new Divisa("franco suizo", 1.18);
+            //ColeccionDivisas colDiv = new ColeccionDivisas();
+            //colDiv.Add(divRef);
+            //colDiv.Add(div1);
+            //colDiv.Add(div2);
+            //colDiv.Add(div3);
+            //colDiv.Add(div4);
+            //Conversor conversor = new Conversor(divRef,colDiv);
+            //Consola c = new Consola(conversor);
+            //c.Menu();
+            //Console.ReadLine();
+
         }
     }
 }
